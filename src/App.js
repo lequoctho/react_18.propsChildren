2e2451a5b8f82bf23595b9df4028159242b78b5b
Glitch (starter-react-template) <none>
2020-06-03 08:48:49 +0000

    🐑🍯 Checkpoint ./src/components/Modal.js:15532837/9261 ./src/components/Modal.css:15532837/686 ./src/App.css:15532837/984 ./src/App.js:15532837/909

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Modal from './components/Modal';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <button>Open modal</button>
-          </p>
-          <a
-            className="App-link"
-            href="https://coders-x.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Yêu anh Thịnh
-          </a>
-        </header>      
+        <Modal/>
       </div>
     );
   }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,2 +1,65 @@
-// 1. Code ra như design https://www.figma.com/file/M2Lh7XaEKWnds9SeEuJ1l0/B%C3%A0i-t%E1%BA%ADp-CSS-cho-entry-test?node-id=87%3A102
-// 2. Thêm prop `content` thay vì hard-code text content trong Modal
+import React, {Component} from 'react';
+import './Modal.css'
+import classNames from 'classnames';
+
+class Modal extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {visiable: true};
+      this.onClickModal = this.onClickModal.bind(this); 
+  }
+
+  onClickModal() {
+    this.setState(state => ({
+      visiable: !state.visiable
+    }));
+  }
+
+  render() {
+      const { visiable } = this.state;
+      return <div> 
+            <input id="myBtn" type="button" onClick={this.onClickModal} value="Modal"/>
+            
+              <div className={classNames('modal-content', {
+                'vis': visiable
+              })}>
+                <div className="modal-header">
+                  <span class="close">&times;</span>
+                  <h2>This is a modal 1</h2>
+                </div>
+                <div className="modal-content">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                    officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
+                    amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                    anim id est laborum.
+                  </p>
+                </div>
+                <div className="modal-footer">
+                  <h3>Modal Footer</h3>
+                </div>
+              </div>
+
+            
+          </div>;
+    }
+}
+
+export default Modal;
